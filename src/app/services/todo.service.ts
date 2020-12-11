046631620c27449a02c4c3a871d1775e611d9d76
ericmcgrandle <ericmcgrandle@gmail.com>
2020-12-10 16:47:50 -0800

    get data from jsonplaceholder using HttpClient

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../models/Todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos'
+  todosLimit:string = '?_limit=5'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'First Task',
-        completed: true
-      },
-      {
-        id: 2,
-        title: 'Second Task',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Third Task',
-        completed: false
-      }
-    ]
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
